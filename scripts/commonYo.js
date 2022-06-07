3d2658b66aaf011afa95bbd787265f825e2c0932
André Elimelek de Weber <38350057+andrekwr@users.noreply.github.com>
2022-06-06 21:48:20 -0300

    Update commonYo.js

--- scripts/commonYo.js
+++ scripts/commonYo.js
@@ -149,7 +149,7 @@ function getUserPoints() {
     }
     return new Promise((resolve, reject) => {
 
-        let getUserPointsUri = "`${getCurrentUserBaseUri()}`/points/get/summary"
+        let getUserPointsUri = `${getCurrentUserBaseUri()}/points/get/summary`
     
         if (devMode) {
             getUserPointsUri = "https://8e9nbq8rj1.execute-api.us-east-2.amazonaws.com/DEV/points/get/summary"
